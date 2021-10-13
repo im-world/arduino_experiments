46aea91f5b9f158597ba832631c52701a025c8b7
im-world <52660201+im-world@users.noreply.github.com>
2021-10-14 00:58:06 +0530

    Added street light control system
    Contains circuit and code

---
+++ Automatic_Street_Light_Control_System.ino
@@ -0,0 +1,28 @@
+#define ldrPin A1
+#define ledPin 4
+
+int ldrValue = 0;
+int lightThreshold = 100;  
+int delayTime = 1000;     //one minute delay
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ldrPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  ldrValue = analogRead(ldrPin);
+  
+  if(ldrValue < lightThreshold){
+    digitalWrite(ledPin, HIGH);
+  }
+  else{
+    digitalWrite(ledPin, LOW);
+  }
+  
+  delay(delayTime);     //read LDR Value after some delay
+
+}
